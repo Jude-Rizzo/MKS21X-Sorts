68aad6a594e5f661e693c309e5a3f3dfbc8b98a6
Jude-Rizzo <jprizzo2020@gmail.com>
2018-12-10 11:48:23 -0500

    :(

--- Sorts.java
+++ Sorts.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Sorts{
 
 
-  public static void selectionsort(int[] ary) {
+  public static void selectionSort(int[] ary) {
 		int min = 0;
     int holder = 0;
 
@@ -20,7 +20,7 @@ public class Sorts{
 			holder = ary[min];
 			ary[min] = ary[start];
 			ary[start] = holder;
-      System.out.println(Arrays.toString(ary));
+      return;
 		}
 
 
@@ -29,8 +29,9 @@ public class Sorts{
 
  public static void bubbleSort(int[] args){
    int holder;
-   boolean s;
+   boolean s = false;
    //s stores if there was a swap, if no swaps were made in the last round we go through
+   if(args.length != 1 && args.length != 0){
    for(int i = args.length - 2; i >= 0;  i--){
      s = false;
      for(int j = 0; j < i; j++){
@@ -40,7 +41,8 @@ public class Sorts{
        args[i + 1] = holder;
        s = true;
      }
-   } if (!s) System.out.println(Arrays.toString(args));
+   }
+ } if (!s)return;
    }
  }
 
