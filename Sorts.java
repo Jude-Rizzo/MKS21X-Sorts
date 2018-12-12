b3f954ac38ac635bf8f718f76cc963d20e609d72
Jude Rizzo <jrizzo00@stuy.edu>
2018-12-11 22:51:42 -0500

    :)

--- Sorts.java
+++ Sorts.java
@@ -2,42 +2,39 @@ import java.util.*;
 public class Sorts{
 
 
-  public static void selectionSort(int[] ary) {
-  		int min = 0;
-      int holder = 0;
-  		for (int start = 0; start < ary.length; start++) {
-  			min = start;
+  public static void selectionSort(int [] data) {
+  for (int i = 0; i < data.length - 1; i++) {
+     int min = i;
 
-  			for (int i = start; i < ary.length; i++) {
-  				if (ary[i] < ary[min]){
-          min = i;
-          holder = ary[min];
-          ary[min] = ary[start];
-          ary[start] = holder;
-        }
+    for (int j = i + 1; j < data.length; j++) {
+      if (data[j] == (Math.min(data[j], data[min]))) {
+        min = j;
+      }
 
-  			}
-
-  		}
-
-
-   }
-
-
- public static void bubbleSort(int[] args){
-  if(args.length!= 0){
-    int b = args.length;
-    for(int i = 0; i < args.length - 1; i++){
-      for(int j = 0; j < b - 1; j = j ++){
-        if (args[j] > args[j + 1]){
-          int holder = args[j];
-          args[j] = args[j + 1];
-          args[j + 1] = holder;
-        }
-      } b -= 1;
-    }
+      }
+    int m = data[min];
+    data [min] = data[i];
+    data[i] = m;
   }
 }
+
+
+public static void bubbleSort(int[] ary) {
+   int holder;
+   boolean s;
+   for (int j = ary.length-1; j >= 0; j--) {
+     s = false;
+     for (int i = 0; i < j; i++) {
+       if (ary[i+1] < ary[i]) {
+         holder = ary[i];
+         ary[i] = ary[i+1];
+         ary[i+1] = holder;
+         s = true;
+       }
+     }
+     if (!s) return;
+   }
+ }
 
 public static void insertionSort(int[] ary){
   for(int i = 1; i < ary.length; i++){
